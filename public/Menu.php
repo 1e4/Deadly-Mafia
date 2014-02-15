0c239f0423747d832803af38933dc32018ca9c6d
Ian <ian@stoopidmonkey.net>
2014-02-15 08:16:05 -0800

    Added simple threaded messaging Changed some links on the menu as to not give 404's

--- public/Menu.php
+++ public/Menu.php
@@ -93,14 +93,14 @@ EOF;
                 </tr>
                 <tr>
                     <td class="menuContent">
-                        <a href="app/communication/inbox.php" target="main">&gt; Inbox</a><br />
-                        <a href="app/communication/send.php" target="main">&gt; Send message</a><br />
-                        <a href="app/communication/forum_frame.php?forum=main" target="main">&gt; Forum</a><br />
-                        <a href="app/communication/forum_frame.php?forum=oc" target="main">&gt; OC Forum</a><br />
-                        <a href="app/communication/ticket.php" target="main">&gt; Help Desk</a><br />
-                        <a href="app/communication/hdo_stats.php" target="main">&gt; HDOP Statistics</a><br />
-                        <a href="app/communication/oeticket.php" target="main">&gt; HDOP Corner</a><br />
-                        <a href="app/communication/forum_frame.php?forum=main" target="main">&gt; Crew Forum</a><br />
+                        <a href="inbox.php" target="main">&gt; Inbox</a><br />
+                        <a href="send.php" target="main">&gt; Send message</a><br />
+                        <a href="forum_frame.php?forum=main" target="main">&gt; Forum</a><br />
+                        <a href="forum_frame.php?forum=oc" target="main">&gt; OC Forum</a><br />
+                        <a href="ticket.php" target="main">&gt; Help Desk</a><br />
+                        <a href="hdo_stats.php" target="main">&gt; HDOP Statistics</a><br />
+                        <a href="oeticket.php" target="main">&gt; HDOP Corner</a><br />
+                        <a href="forum_frame.php?forum=main" target="main">&gt; Crew Forum</a><br />
                     </td>
                 </tr>
                 <!-- /communication -->
@@ -110,28 +110,28 @@ EOF;
                 </tr>
                 <tr>
                     <td class="menuContent">
-                        <a href="app/criminal/crime.php" target="main">&gt; Crimes</a><br />
-                        <a href="app/criminal/chase.php" target="main">&gt; Getaway</a><br />
-                        <a href="app/criminal/jack.php" target="main">&gt; GTA</a><br />
-                        <a href="app/criminal/jail.php" target="main">&gt; Jail</a><br />
-                        <a href="app/criminal/oc.php" target="main">&gt; OC</a><br />
-                        <a href="app/criminal/ext.php" target="main">&gt; Extortion</a><br />
-                        <a href="app/criminal/drugs.php" target="main">&gt; Drug Cartel</a><br />
-                        <a href="app/criminal/fly.php" target="main">&gt; Travel</a><br />
-                        <a href="app/criminal/bf.php" target="main">&gt; Bullet Factory</a><br />
-                        <a href="app/criminal/bank.php" target="main">&gt; Bank</a><br />
-                        <a href="app/criminal/hospital.php" target="main">&gt; Hospital</a><br />
-                        <a href="app/criminal/kill.php" target="main">&gt; Search & Kill</a><br />
-                        <a href="app/criminal/safehouse.php" target="main">&gt; Safehouse</a><br />
-                        <a href="app/criminal/crew.php" target="main">&gt; Crew</a><br />
-                        <a href="app/criminal/buy_crew.php" target="main">&gt; Make Crew</a><br />
-                        <a href="app/criminal/crew_cp.php" target="main">&gt; Crew Control Panel</a><br />
-                        <a href="app/criminal/blackmarket.php" target="main">&gt; Blackmarket</a><br />
-                        <a href="app/criminal/auctions.php" target="main">&gt; Bidding Center</a><br />
-                        <a href="app/criminal/garage.php" target="main">&gt; Garage</a><br />
-                        <a href="app/criminal/streetracing.php" target="main">&gt; Street Race</a><br />
-                        <a href="app/criminal/dealership.php" target="main">&gt; Show Room</a><br />
-                        <a href="app/criminal/depot.php" target="main">&gt; Car Mods</a></span></td>
+                        <a href="crime.php" target="main">&gt; Crimes</a><br />
+                        <a href="chase.php" target="main">&gt; Getaway</a><br />
+                        <a href="jack.php" target="main">&gt; GTA</a><br />
+                        <a href="jail.php" target="main">&gt; Jail</a><br />
+                        <a href="oc.php" target="main">&gt; OC</a><br />
+                        <a href="ext.php" target="main">&gt; Extortion</a><br />
+                        <a href="drugs.php" target="main">&gt; Drug Cartel</a><br />
+                        <a href="fly.php" target="main">&gt; Travel</a><br />
+                        <a href="bf.php" target="main">&gt; Bullet Factory</a><br />
+                        <a href="bank.php" target="main">&gt; Bank</a><br />
+                        <a href="hospital.php" target="main">&gt; Hospital</a><br />
+                        <a href="kill.php" target="main">&gt; Search & Kill</a><br />
+                        <a href="safehouse.php" target="main">&gt; Safehouse</a><br />
+                        <a href="crew.php" target="main">&gt; Crew</a><br />
+                        <a href="buy_crew.php" target="main">&gt; Make Crew</a><br />
+                        <a href="crew_cp.php" target="main">&gt; Crew Control Panel</a><br />
+                        <a href="blackmarket.php" target="main">&gt; Blackmarket</a><br />
+                        <a href="auctions.php" target="main">&gt; Bidding Center</a><br />
+                        <a href="garage.php" target="main">&gt; Garage</a><br />
+                        <a href="streetracing.php" target="main">&gt; Street Race</a><br />
+                        <a href="dealership.php" target="main">&gt; Show Room</a><br />
+                        <a href="depot.php" target="main">&gt; Car Mods</a></span></td>
                     </td>
                 </tr>
                 <!-- /actions -->
@@ -141,12 +141,12 @@ EOF;
                 </tr>
                 <tr>
                     <td class="menuContent">
-                        <a href="app/gambling/keno.php" target="main">&gt; Keno</a><br />
-                        <a href="app/gambling/blackjack.php" target="main">&gt; BlackJack</a><br />
-                        <a href="app/gambling/race.php" target="main">&gt; Race Track</a><br />
-                        <a href="app/gambling/roulette.php" target="main">&gt; Roulette</a><br />
-                        <a href="app/gambling/slots.php" target="main">&gt; Slots</a><br />
-                        <a href="app/gambling/lotto.php" target="main">&gt; Lottery</a><br/>
+                        <a href="keno.php" target="main">&gt; Keno</a><br />
+                        <a href="blackjack.php" target="main">&gt; BlackJack</a><br />
+                        <a href="race.php" target="main">&gt; Race Track</a><br />
+                        <a href="roulette.php" target="main">&gt; Roulette</a><br />
+                        <a href="slots.php" target="main">&gt; Slots</a><br />
+                        <a href="lotto.php" target="main">&gt; Lottery</a><br/>
                     </td>
                 </tr>
                 <!-- /gambling -->
